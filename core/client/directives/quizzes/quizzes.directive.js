11b68f9133a2bfdd4208543c243e59ade7a22f60
Ladrillo <gabriel@cabrejas.net>
2015-08-24 17:08:04 -0600

    refactoring successful

--- core/client/directives/quizzes/quizzes.directive.js
+++ core/client/directives/quizzes/quizzes.directive.js
@@ -8,7 +8,7 @@
 
                 templateUrl: 'directives/quizzes/quizzes.template.html',
 
-                controller: function ($scope) {
+                controller: function ($scope, subjectsResource, keywordsResource, quizzesResource) {
                     // words directive is bound to these:
                     $scope.checkedSubjects = [];
                     $scope.checkedKeywords = [];
@@ -29,6 +29,96 @@
                     $scope.editing = false;
                     $scope.flipEditing = function () {
                         this.editing = !this.editing;
+                    };
+
+                    $scope.subjectKind = "Subjects";
+
+                    // GET all
+                    $scope.listSubjects = function () {
+                        $scope.subjects = subjectsResource.query();
+                    };
+
+                    // DELETE
+                    $scope.deleteSubject = function () {
+                        $scope.currentSubject.$delete();
+                    };
+
+                    // POST
+                    $scope.createSubject = function () {
+                        new subjectsResource({ name: $scope.newSubject })
+                            .$save()
+                            .then(function (newWord) {
+                                $scope.subjects.push(newWord);
+                            });
+                    };
+
+                    // PUT
+                    $scope.updateSubject = function () {
+                        subjectsResource.update({ _id: $scope.currentSubject._id }, $scope.currentSubject);
+                    };
+
+
+                    $scope.listSubjects(); // populate view
+
+
+                    // KEYWORDS KEYWORDS KEYWORDS KEYWORDS KEYWORDS
+                    // KEYWORDS KEYWORDS KEYWORDS KEYWORDS KEYWORDS
+                    // KEYWORDS KEYWORDS KEYWORDS KEYWORDS KEYWORDS
+
+                    $scope.keywordKind = "Keywords";
+
+                    // GET all
+                    $scope.listKeywords = function () {
+                        $scope.keywords = keywordsResource.query();
+                    };
+
+                    // DELETE
+                    $scope.deleteKeyword = function () {
+                        $scope.currentKeyword.$delete();
+                    };
+
+                    // POST
+                    $scope.createKeyword = function () {
+                        new keywordsResource({ name: $scope.newKeyword })
+                            .$save()
+                            .then(function (newWord) {
+                                $scope.keywords.push(newWord);
+                            });
+                    };
+
+                    // PUT
+                    $scope.updateKeyword = function () {
+                        keywordsResource.update({ _id: $scope.currentKeyword._id }, $scope.currentKeyword);
+                    };
+
+
+                    $scope.listKeywords(); // populate view
+
+
+                    // QUIZZES QUIZZES QUIZZES QUIZZES QUIZZES
+                    // QUIZZES QUIZZES QUIZZES QUIZZES QUIZZES
+                    // QUIZZES QUIZZES QUIZZES QUIZZES QUIZZES
+
+                    function extractCheckedNames(objArray) {
+                        var names = [];
+                        for (var i = 0; i < objArray.length; i++) {
+                            names.push(objArray[i]['name']);
+                        }
+                        return names;
+                    }
+
+
+                    $scope.createQuiz = function () {
+                        new quizzesResource({
+                            subjects: extractCheckedNames($scope.checkedSubjects),
+                            keywords: extractCheckedNames($scope.checkedKeywords),
+                            instructions: $scope.instructions,
+                            stem: $scope.stem,
+                            truthies: $scope.truthies,
+                            falsies: $scope.falsies,
+                            regexps: $scope.regexps
+                        })
+                            .$save();
                     };
                 }
             };
