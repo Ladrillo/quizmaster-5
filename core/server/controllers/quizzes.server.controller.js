425e689263a84967fb5d68859ad2b1344c46e5ce
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-08-27 11:05:39 -0600

    put endpoint for quizzes still doesn't work

--- core/server/controllers/quizzes.server.controller.js
+++ core/server/controllers/quizzes.server.controller.js
@@ -25,6 +25,9 @@ exports.getQuizzes = function (req, res) {
 
 exports.getOneQuiz = function (req, res) {
     Quiz.findById(req.params.id)
+        .populate('user')
+        .populate('subjects')
+        .populate('keywords')
         .exec(function (err, quiz) {
             if (err) res.status(500).send(err);
             else res.json(quiz);
@@ -37,7 +40,13 @@ exports.putQuiz = function (req, res) {
         .exec(function (err, quiz) {
             if (err) res.status(500).send(err);
             else {
-                quiz.name = req.body.name;
+                quiz.subjects = req.body.subjects;
+                quiz.keywords = req.body.keywords;
+                quiz.instructions = req.body.instructions;
+                quiz.stem = req.body.stem;
+                quiz.truthies = req.body.truthies;
+                quiz.falsies = req.body.falsies;
+                quiz.regexps = req.body.regexps;
                 quiz.save();
                 res.json(quiz);
             }
