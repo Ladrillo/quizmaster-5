e62028417656fdd403cbc997328e896491f7dc5d
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2015-08-31 15:10:47 -0600

    adios a quizfull directive consumado

--- core/client/components/quizedit/quiz.edit.client.controller.js
+++ core/client/components/quizedit/quiz.edit.client.controller.js
@@ -120,6 +120,82 @@
             this.submitted = true;
         };
 
+
+        // WORDS DIRECTIVE
+        // WORDS DIRECTIVE
+        // WORDS DIRECTIVE
+        
+        $scope.truthiesType = "Truthy answers";
+        $scope.falsiesType = "Falsy answers";
+        $scope.regexpsType = "Regular Expressions";
+
+        $scope.editing = false;
+        $scope.flipEditing = function () {
+            this.editing = !this.editing;
+        };
+
+
+        // SUBJECTS SUBJECTS SUBJECTS SUBJECTS SUBJECTS
+        // SUBJECTS SUBJECTS SUBJECTS SUBJECTS SUBJECTS
+
+        $scope.subjectKind = "Subjects";
+
+        // GET all
+        $scope.listSubjects = function () {
+            return this.subjects; // this comes from parent controller
+        };
+
+        // DELETE
+        $scope.deleteSubject = function () {
+            $scope.currentSubject.$delete();
+        };
+
+        // POST
+        $scope.createSubject = function () {
+            new subjectsResource({ name: $scope.newSubject })
+                .$save()
+                .then(function (newWord) {
+                    $scope.subjects.push(newWord);
+                });
+        };
+
+        // PUT
+        $scope.updateSubject = function () {
+            subjectsResource.update({ _id: $scope.currentSubject._id }, $scope.currentSubject);
+        };
+
+
+        // KEYWORDS KEYWORDS KEYWORDS KEYWORDS KEYWORDS
+        // KEYWORDS KEYWORDS KEYWORDS KEYWORDS KEYWORDS
+
+        $scope.keywordKind = "Keywords";
+
+        // GET all
+        $scope.listKeywords = function () {
+            return this.keywords; // this comes from parent controller
+        };
+
+        // DELETE
+        $scope.deleteKeyword = function () {
+            $scope.currentKeyword.$delete();
+        };
+
+        // POST
+        $scope.createKeyword = function () {
+            new keywordsResource({ name: $scope.newKeyword })
+                .$save()
+                .then(function (newWord) {
+                    $scope.keywords.push(newWord);
+                });
+        };
+
+        // PUT
+        $scope.updateKeyword = function () {
+            keywordsResource.update({ _id: $scope.currentKeyword._id }, $scope.currentKeyword);
+        };
+
+
+
     }
 
 
